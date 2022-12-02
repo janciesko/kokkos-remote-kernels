d9c831de49a2e760e4946d27496fa93a015d576c
Jan Ciesko <jan.ciesko@gmail.com>
2022-12-02 15:55:52 -0700

    Minor API usage updates

--- KokkosRemoteKernels_norm.cpp
+++ KokkosRemoteKernels_norm.cpp
@@ -3,41 +3,36 @@
 #include "Kokkos_RemoteSpaces.hpp"
 
 #include <mpi.h>
+#include <networking_code.hpp>
 
 using RemoteSpace_t  = Kokkos::Experimental::DefaultRemoteMemorySpace;
+#define SIZE 1024
 
 int main(int argc, char *argv[]) {
-  // MPI
-  int myRank, numRanks;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-  MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
 
-#ifdef KOKKOS_ENABLE_SHMEMSPACE
-  shmem_init();
-#endif
-#ifdef KOKKOS_ENABLE_NVSHMEMSPACE
-  MPI_Comm mpi_comm;
-  nvshmemx_init_attr_t attr;
-  mpi_comm      = MPI_COMM_WORLD;
-  attr.mpi_comm = &mpi_comm;
-  nvshmemx_init_attr(NVSHMEMX_INIT_WITH_MPI_COMM, &attr);
-#endif
+  networking_init(argc, argv);
 
   Kokkos::initialize(argc, argv);
   {
+    int myRank;
+    int numRanks;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numRanks);
+    
     printf("Hello from rank %d!\n", myRank);
-
-    const int nx_per_rank = 16;
-    const int nx_total    = nx_per_rank*numRanks;
-
+    int nx_total     = SIZE;
+    auto local_range = Kokkos::Experimental::get_range(nx_total, myRank);
+    
     Kokkos::View<double*, RemoteSpace_t> myVector("vector", nx_total);
-    Kokkos::parallel_for(Kokkos::RangePolicy(myRank*nx_per_rank, (myRank + 1)*nx_per_rank),
-			 KOKKOS_LAMBDA(const int idx) {myVector(idx) = (double)(myRank + 1);});
+    Kokkos::parallel_for(Kokkos::RangePolicy(local_range.second, local_range.first),
+			 KOKKOS_LAMBDA(const int idx) {     
+         myVector(idx) = (double)(myRank + 1);
+    });
 
     printf("rank %d: myVector.extent(0)=%d, nx_total=%d\n", myRank, int(myVector.extent(0)), int(nx_total));
-
     const double norm = KokkosBlas::nrm2_squared(myVector);
   }
-  Kokkos::finalize();
+    Kokkos::finalize();
+    networking_fin();
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# kokkos-remote-kernels
+# Kokkos Remote Kernels
+
+This repository contains sample codes that shows how Kokkos remote space and Kokkos Kernels can be used to implement distribute linear algebra kernels using PGAS as the primary communication layer.
+
+### Building Kokkos Remote Kernels
+
+First you will need to build Kokkos, Kokkos Kernels and Kokkos remote spaces using a consistent set of compilers.
+Once all are installed you can configure using CMake as follows
+
+```
+cmake -S path/to/kokkos_remote_kernels/source \
+    -B path/to/the/build/directory \
+    -D CMAKE_CXX_COMPILER="same compiler as that used with Kokkos remote spaces" \
+    -D KokkosKernels_ROOT:PATH="path/to/KokkosKernels/installation" \
+    -D KokkosRemote_ROOT:PATH="path/to/Kokkos_Remote_Spaces/installation"
+```
